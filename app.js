7b92e2dc9f96e11dc5a11faeed315a7804fd45d7
Sonmung <ksmatoz@gmail.com>
2022-10-26 14:25:09 +0530

    installed discordjs

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
+require('dotenv').config();
 const express = require('express');
+const { REST, Client, Routes, GatewayIntentBits } = require('discord.js');
 const app = express();
+const TOKEN = process.env.TOKEN;
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 
 app.get('/home', (req,res) => {
     res.send('Hello to my home')
 })
+
+async function main() {
+    try {
+      await client.login(TOKEN);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  main();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT)
